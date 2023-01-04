8c1af584bcb8f0a1eed414cc94c28bccfb181e5e
JP <joao.santos-ctr@vectorsolutions.com>
2023-01-03 22:23:01 -0300

    create backend module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BackendModule } from './backend/backend.module';
 
 @Module({
-  imports: [],
+  imports: [BackendModule],
   controllers: [AppController],
   providers: [AppService],
 })
